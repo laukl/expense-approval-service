d2163811e9f3b203edcc341cd63df2162fbcf574
M Lau <63963445+tofuuudon@users.noreply.github.com>
2025-05-01 17:30:34 +0100

    refactor: seed script

--- src/scripts/seed.ts
+++ src/scripts/seed.ts
@@ -2,69 +2,73 @@
 
 import { PrismaClient, User } from "@prisma/client";
 
-const USERS: Omit<User, "id" | "createdAt" | "updatedAt" | "managerId">[] = [
+const USERS: Omit<User, "createdAt" | "updatedAt">[] = [
   {
+    id: "cma52ov6y0000jmltymeilpum",
     email: "jof@tipalti.com",
     role: "MANAGER",
+    managerId: null,
   },
   {
+    id: "cma52ov6y0001jmltz9wpsrie",
     email: "tom@tipalti.com",
     role: "MANAGER",
+    managerId: "cma52ov6y0000jmltymeilpum",
   },
   {
+    id: "cma52ov6y0002jmltz63w23sz",
     email: "nico@tipalti.com",
     role: "MANAGER",
+    managerId: "cma52ov6y0000jmltymeilpum",
   },
   {
+    id: "cma52ov6y0003jmlt0e729uyf",
     email: "ori@tipalti.com",
     role: "MANAGER",
+    managerId: "cma52ov6y0000jmltymeilpum",
   },
   {
+    id: "cma52ov6y0004jmltdj1zd4z3",
     email: "igor@tipalti.com",
     role: "MANAGER",
+    managerId: "cma52ov6y0000jmltymeilpum",
   },
   {
+    id: "cma52ov6y0005jmlt69tgezh4",
     email: "sergey@approve.com",
     role: "MANAGER",
+    managerId: "cma52ov6y0001jmltz9wpsrie",
   },
   {
+    id: "cma52ov6y0006jmltun1kleco",
     email: "reut@approve.com",
     role: "MANAGER",
+    managerId: "cma52ov6y0001jmltz9wpsrie",
   },
   {
+    id: "cma52ov6y0007jmltn7wl1qp1",
     email: "ben@approve.com",
     role: "MANAGER",
+    managerId: "cma52ov6y0001jmltz9wpsrie",
   },
   {
+    id: "cma52ov6y0008jmltpok252pl",
     email: "michael-finance@tipalti.com",
     role: "FINANCE",
+    managerId: null,
   },
 ];
 
 const prisma = new PrismaClient();
 
-async function run() {
-  const createResult = await prisma.user.createMany({ data: USERS });
-  console.log(`Created: ${createResult.count}`);
-
-  const users = await prisma.user.findMany({
-    where: { role: "MANAGER", managerId: null },
-  });
-  const userIds = users.map((user) => user.id);
-
-  return prisma.$transaction(
-    users.map((user) => {
-      const userIdsNotThisUser = userIds.filter((id) => id !== user.id);
-      const managerId =
-        userIdsNotThisUser[
-          Math.floor(Math.random() * userIdsNotThisUser.length)
-        ];
-      return prisma.user.update({
-        where: { id: user.id },
-        data: { managerId },
-      });
-    }),
-  );
-}
-
-run().then(console.log);
+prisma
+  .$transaction(
+    USERS.map((user) =>
+      prisma.user.upsert({
+        where: { email: user.email },
+        create: user,
+        update: user,
+      }),
+    ),
+  )
+  .then(console.log);
