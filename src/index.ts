d7444c009f142865fa8cd022d82ac6859a71960f
M Lau <63963445+tofuuudon@users.noreply.github.com>
2025-05-01 17:36:56 +0100

    chore: add comments on index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log("Hello");
+console.log("Hello, World!");
+
+// If I had more time, this is where an Express.js server would be implemented
+//
+// I would use index.ts as the entrypoint and the service would be Dockerized via a multi-stage build to reduce image size.
+// The image would run as a non-root user, e.g. node.
